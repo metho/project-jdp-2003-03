64870459227f47b48970ac74c0d3f76c2723cea5
KariFra <mystery.tomorrow.2@gmail.com>
2020-03-26 10:54:55 +0100

    changes to order controller

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,9 +1,7 @@
 package com.kodilla.ecommercee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,23 +17,23 @@ public class OrderController {
     @Autowired
     private OrderMapper orderMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    @GetMapping(value = "getOrders")
     public List<OrderDto> getOrders(){
         return orderMapper.translateToOrderList(service.getOrders());
     }
-    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    @PostMapping(value = "createOrder")
     public void createOrder(OrderDto orderDto){
         service.saveOrder(orderMapper.translateToOrder(orderDto));
     }
-    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
-    public OrderDto getOrder(Long orderId) throws OrderNotFoundException {
+    @GetMapping(value = "/{orderId}")
+    public OrderDto getOrder(@PathVariable Long orderId) throws OrderNotFoundException {
         return orderMapper.translateToOrderDto(service.getOrder(orderId).orElseThrow(OrderNotFoundException::new));
     }
-    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    @PutMapping(value = "updateOrder")
     public OrderDto updateOrder(ProductOrder order){
         return orderMapper.translateToOrderDto(service.saveOrder(order));
     }
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    @DeleteMapping(value = "deleteOrder")
     public void deleteOrder(Long orderId){
         service.deleteOrder(orderId);
     }
