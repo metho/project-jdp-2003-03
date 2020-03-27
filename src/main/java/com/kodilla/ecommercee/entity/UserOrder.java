341b714c56021fbe78439304b1360f06bf7b7490
KariFra <mystery.tomorrow.2@gmail.com>
2020-03-27 08:56:47 +0100

    UserOrder entity-first commit

--- src/main/java/com/kodilla/ecommercee/entity/UserOrder.java
+++ src/main/java/com/kodilla/ecommercee/entity/UserOrder.java
@@ -1,13 +1,15 @@
 package com.kodilla.ecommercee.entity;
 
 
+import com.kodilla.ecommercee.GenericEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,4 +21,17 @@ public class UserOrder {
     @Id
     @GeneratedValue
     private Long Id;
+
+    private Date orderMade;
+
+    private Boolean resolved;
+
+    @OneToOne(cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
+    @JoinColumn
+    private GenericEntity user;
+
+    @OneToMany
+    @JoinColumn
+    private List<GenericEntity> products;
 }
