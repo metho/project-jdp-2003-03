ae2b01830a7bf08c9b1d477df6030ab1107d8379
KariFra <mystery.tomorrow.2@gmail.com>
2020-03-27 14:03:18 +0100

    changes to entity UserOrder after first review

--- src/main/java/com/kodilla/ecommercee/entity/UserOrder.java
+++ src/main/java/com/kodilla/ecommercee/entity/UserOrder.java
@@ -1,14 +1,12 @@
 package com.kodilla.ecommercee.entity;
 
-
-import com.kodilla.ecommercee.GenericEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -22,16 +20,16 @@ public class UserOrder {
     @GeneratedValue
     private Long Id;
 
-    private Date orderMade;
+    private LocalDate orderMade;
 
-    private Boolean resolved;
+    private boolean resolved;
 
     @OneToOne(cascade = CascadeType.ALL,
             fetch = FetchType.EAGER)
     @JoinColumn
-    private GenericEntity user;
+    private User userId;
 
     @OneToMany
     @JoinColumn
-    private List<GenericEntity> products;
+    private List<Product> products;
 }
