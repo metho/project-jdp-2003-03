cf8771244dd2686965b226f4dae51eefbef031c2
Aleks0491 <aleksander.sotor@gmail.com>
2020-03-29 01:11:02 +0100

    Encja Product - implementacja

--- src/main/java/com/kodilla/ecommercee/entity/Product.java
+++ src/main/java/com/kodilla/ecommercee/entity/Product.java
@@ -1,23 +1,41 @@
 package com.kodilla.ecommercee.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(unique = true)
     private Long id;
+
+    @Column
+    @NotNull
+    private String name;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String model;
+
+    @Column(name = "Production_year")
+    private int year;
+
+    @Column
+    private String origin;
+
+    @Column
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "product_group_id", nullable = false)
@@ -25,4 +43,6 @@ public class Product {
 
     @OneToMany(mappedBy = "product")
     private List<Item> items = new ArrayList<>();
+
+
 }
