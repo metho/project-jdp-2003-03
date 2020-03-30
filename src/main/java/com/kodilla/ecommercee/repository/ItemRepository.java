dd31eccf63ba19d248ec4838850f6b527228d19b
binkul <jacekbinkul@gmail.com>
2020-03-30 14:57:39 +0200

    Encja cart testy

---
+++ src/main/java/com/kodilla/ecommercee/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
---
+++ src/test/java/com/kodilla/ecommercee/entity/CartRepositoryTest.java
@@ -0,0 +1,141 @@
+package com.kodilla.ecommercee.entity;
+
+import com.kodilla.ecommercee.repository.CartRepository;
+import static org.junit.Assert.*;
+
+import com.kodilla.ecommercee.repository.ItemRepository;
+import com.kodilla.ecommercee.repository.ProductGroupRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CartRepositoryTest {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    ProductGroupRepository groupRepository;
+
+    @Before
+    public void printBefore() {
+        System.out.println("\nStart test");
+    }
+
+    @Test
+    public void testSaveAndGetCart() {
+        // Given
+        Cart cartA = new Cart();
+        Cart cartB = new Cart();
+        Cart cartC = new Cart();
+
+        // When
+        System.out.println("Test get all records ...\n");
+        cartRepository.save(cartA);
+        cartRepository.save(cartB);
+        cartRepository.save(cartC);
+
+        List<Cart> carts = cartRepository.findAll();
+
+        // Then
+        assertTrue(carts.size() >= 3);
+        assertTrue(carts.contains(cartB));
+    }
+
+    @Test
+    public void testUpdateCart() {
+        // Given
+        Cart cartA = new Cart();
+        Cart cartB = new Cart();
+        Cart cartC = new Cart();
+
+        // When
+        System.out.println("Test update records ...\n");
+        cartRepository.save(cartA);
+        cartRepository.save(cartB);
+        cartRepository.save(cartC);
+
+        Long id = cartB.getId();
+        Cart cart = cartRepository.findById(id).orElseThrow(RuntimeException::new);
+        cart.setClosed(true);
+        cartRepository.save(cart);
+        Cart found = cartRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        // Then
+        assertTrue(found.isClosed());
+    }
+
+    @Test
+    public void testDeleteCart() {
+        // Given
+        Cart cartA = new Cart();
+        Cart cartB = new Cart();
+        Cart cartC = new Cart();
+
+        // When
+        System.out.println("Test delete records ...\n");
+        cartRepository.save(cartA);
+        cartRepository.save(cartB);
+        cartRepository.save(cartC);
+
+        Long id = cartB.getId();
+        cartRepository.deleteById(id);
+        Optional<Cart> found = cartRepository.findById(id);
+
+        // Then
+        assertFalse(found.isPresent());
+    }
+
+    @Test
+    public void testCartAndItem() {
+        // Given
+        Cart cart = new Cart();
+        Product product = new Product();
+        ProductGroup group = new ProductGroup("Empty");
+        Item itemA = new Item(cart, product, 2.0, 12.4);
+        Item itemB = new Item(cart, product, 1.0, 10.0);
+        Item itemC = new Item(cart, product, 0.5, 5.0);
+        cart.getItems().addAll(Arrays.asList(itemA, itemB, itemC));
+        product.setProductGroup(group);
+        product.getItems().addAll(Arrays.asList(itemA, itemB, itemC));
+        group.getProducts().add(product);
+
+        // When
+        groupRepository.save(group);
+        productRepository.save(product);
+        cartRepository.save(cart);
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+        itemRepository.save(itemC);
+
+        Long id = cart.getId();
+        Optional<Cart> foundCart = cartRepository.findById(id);
+        List<Item> items = itemRepository.findAll();
+
+        Long idDel = itemB.getId();
+        itemRepository.deleteById(idDel);
+        List<Item> itemsDel = itemRepository.findAll();
+
+        // Then
+        assertEquals(3, items.size());
+        assertTrue(foundCart.isPresent());
+        assertEquals(3, foundCart.get().getItems().size());
+        assertEquals(2, itemsDel.size());
+    }
+}
