fc31960b74a2d49e65f5496fe7a27830f0221a46
Aleks0491 <56281378+Aleks0491@users.noreply.github.com>
2020-04-01 16:26:56 +0200

    JDP200303-12 (#19)
    * Encja Product - implementacja
    
    * Encja Product + ProductDto kosmetyka
    
    * JDP200303-12
    
    * ?
    
    * Nadpisane pliki
    
    * JDP200303-12 v.next
    
    * Next one
    
    * Before mast
    
    * V. next

--- src/main/java/com/kodilla/ecommercee/entity/Product.java
+++ src/main/java/com/kodilla/ecommercee/entity/Product.java
@@ -1,23 +1,31 @@
 package com.kodilla.ecommercee.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private double price;
+    private String brand;
+    private String model;
+    private int year;
+    private String origin;
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "product_group_id", nullable = false)
@@ -26,7 +34,27 @@ public class Product {
     @OneToMany(mappedBy = "product")
     private List<Item> items = new ArrayList<>();
 
+    public Product(Long id, String name, String description, double price, String brand, String model, String origin, int year) {
+    }
+
+    public Product(Long productId, ProductGroup devices, ArrayList<Item> items) {
+        this.id = productId;
+        this.productGroup = devices;
+        this.items = items;
+    }
+
+    public void setProductGroup(ProductGroup productGroup) {
+        this.productGroup = productGroup;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
     public Product(ProductGroup productGroup) {
         this.productGroup=productGroup;
     }
+
 }
--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
