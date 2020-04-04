9aee9b7ba77fb2163d233e148e5cdb4625d5a72a
kaczorowski91@gmail.com <kaczorowski91@gmail.com>
2020-04-04 17:32:34 +0200

    JDP200303-20

---
+++ src/test/java/com/kodilla/ecommercee/service/EmailServiceTest.java
@@ -0,0 +1,42 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.entity.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //Given
+        Mail mail = new Mail("test@test.com","Test","Test Message");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setText(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //When
+        emailService.send(mail);
+        //Then
+        verify(javaMailSender,times(0)).send(mailMessage);
+    }
+
+
+
+}
