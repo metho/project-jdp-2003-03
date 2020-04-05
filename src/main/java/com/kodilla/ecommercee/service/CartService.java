4dd443d641cf36eb4100eca2284b8f5e19e0ecfd
KariFra <mystery.tomorrow.2@gmail.com>
2020-04-05 19:03:39 +0200

    changes after last review part2

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -6,12 +6,11 @@ import com.kodilla.ecommercee.entity.UserOrder;
 import com.kodilla.ecommercee.entity.Cart;
 import com.kodilla.ecommercee.exception.CartNotFoundException;
 import com.kodilla.ecommercee.exception.UserNotFoundException;
-import com.kodilla.ecommercee.mapper.CartMapper;
-import com.kodilla.ecommercee.mapper.OrderMapper;
 import com.kodilla.ecommercee.repository.CartRepository;
 import com.kodilla.ecommercee.repository.ItemRepository;
 import com.kodilla.ecommercee.repository.OrderRepository;
 import com.kodilla.ecommercee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -19,17 +18,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CartService {
 
+    @Autowired
     private CartRepository cartRepository;
-
+    @Autowired
     private ItemRepository itemRepository;
-
+    @Autowired
     private OrderRepository orderRepository;
-
+    @Autowired
     private UserRepository userRepository;
 
-    private CartMapper cartMapper;
-
-    private OrderMapper orderMapper;
 
 
 
